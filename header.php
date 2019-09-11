1c5625498d72c24f3b9afaabda723d8c6fd8dfc3
hardyt46 <55080422+hardyt46@users.noreply.github.com>
2019-09-12 00:29:54 +0530

    Update Header File
    Please copy this code in the header.php file

--- header.php
+++ header.php
@@ -20,7 +20,6 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
 <?php wp_head(); ?>
-<img src = "http://yeezyideationcenter.com/wp-content/themes/gatestone2017/gatestone-logo-1000.gif" />
 </head>
 
 <body <?php body_class(); ?>>
@@ -29,10 +28,18 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentyseventeen' ); ?></a>
 
 	<header id="masthead" class="site-header" role="banner">
-
+		<div class="logo-section" style="width: 1120px;background: #fff;margin: 0 auto;padding: 15px 0;">
+		<div class="inner?" style="width: 1120px;margin: 0 auto -7px;">
+			<img src="http://yeezyideationcenter.com/wp-content/themes/gatestone2017/gatestone-logo-1000.gif" class="custom-logo" alt="" style="width: 20%;">
+			</div>
+	
+		</div>
+	
+		
 		<?php get_template_part( 'template-parts/header/header', 'image' ); ?>
 
 		<?php if ( has_nav_menu( 'top' ) ) : ?>
+	
 			<div class="navigation-top">
 				<div class="wrap">
 					<?php get_template_part( 'template-parts/navigation/navigation', 'top' ); ?>
@@ -42,19 +49,8 @@
 
 	</header><!-- #masthead -->
 
-	<?php
-
-	/*
-	 * If a regular post or page, and not the front page, show the featured image.
-	 * Using get_queried_object_id() here since the $post global may not be set before a call to the_post().
-	 */
-	if ( ( is_single() || ( is_page() && ! twentyseventeen_is_frontpage() ) ) && has_post_thumbnail( get_queried_object_id() ) ) :
-		echo '<div class="single-featured-image-header">';
-		echo get_the_post_thumbnail( get_queried_object_id(), 'twentyseventeen-featured-image' );
-		echo '</div><!-- .single-featured-image-header -->';
-	endif;
-	?>
+	
 
 	<div class="site-content-contain">
 		<div id="content" class="site-content">
-header.php
+
